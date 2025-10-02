fd498df0df9f66fd621bba9a1ef16ec625507338
Mark Matakili <markmatakili25@gmail.com>
2025-10-02 10:05:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6831699418-66c4f",
+  "appId": "1:471535560039:web:ede3b47c445fde5b35fbc2",
+  "apiKey": "AIzaSyDfDrVNGGYm2OFVo38x0XLofm5mQKT5TeY",
+  "authDomain": "studio-6831699418-66c4f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "471535560039"
+};
