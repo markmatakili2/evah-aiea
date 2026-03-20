59959396d658dad02668c41817918912d3caaefd
Mark Matakili <markmatakili25@gmail.com>
2026-03-20 22:23:12 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,8 @@
-/**
- * @fileOverview Firebase configuration and initialization.
- */
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyAz-REPLACED-WITH-VALID-KEY",
-  authDomain: "nextn-40ed1.firebaseapp.com",
-  projectId: "nextn-40ed1",
-  storageBucket: "nextn-40ed1.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890",
+  "projectId": "aiea-project-70021866-ecca1",
+  "appId": "1:504596307491:web:03fa744445b9750208db96",
+  "apiKey": "AIzaSyCy6Ju1YOd7wa7FiRHMF8urgEfiL7oZbW8",
+  "authDomain": "aiea-project-70021866-ecca1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "504596307491"
 };
