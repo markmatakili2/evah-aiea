e504a021e12b0b8e7fc91f3ae2115516d5fd66a9
markmatakili2 <111102024+markmatakili2@users.noreply.github.com>
2026-03-23 23:06:46 +0300

    Deleted client-provider.tsx

--- src/firebase/errors.ts
+++
@@ -1,7 +0,0 @@
-
-export class FirestorePermissionError extends Error {
-  constructor(message: string = 'Permission denied to Firestore resource.') {
-    super(message);
-    this.name = 'FirestorePermissionError';
-  }
-}
--- src/firebase/index.ts
+++
@@ -1,8 +0,0 @@
-
-export * from './config';
-export * from './client-provider';
-export * from './auth/use-user';
-export * from './auth/use-auth';
-export * from './firestore/use-firestore';
-export * from './firestore/use-doc';
-export * from './firestore/use-collection';
